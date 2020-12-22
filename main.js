6563167297e59852ac5e228c75cc03d9dcd6feea
Patosaurio-o <pato.sepulveda@hotmail.es>
2020-12-22 20:09:46 -0300

    finish him

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 $('document').ready(function() {
   for(let i=1; i <= 151; i++){
-    $.get(`https://pokeapi.co/api/v2/pokemon/${i}/`, function(pokemon){
-      let nombre = pokemon.name;
-      let list = '<h6>Type:</h6> <ul>';
-      for(let t=0;t<pokemon.types.length;t++){
-        list += '<li>'+ pokemon.types[t].type.name +'</li>';
-      }
-      list += '</ul>'
       $('#poke-ball').append(
         `<div class="poke-algo">
           <img id="${i}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png"></img>
         </div>`
-        );
-      $('img').on('click', function(){
-        var pokeId = $.attr('id');
-        $('#mostar-pokemon').html(`<p class="mostrarNombre">${nombre}</p>
-        <img id="${pokeId}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png"></img>
-        <p class="mostrarLista">${list}</p>`
-        ); 
-        console.log(pokeId)
-      });
-    })
-  };
-});
-
-
-
+      );
+  }
+  $('img').on('click', function(){
+    let pokeId = $(this).attr('id');
+    console.log(pokeId);
+    $.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`, function(pokemon){
+      let nombre = pokemon.name;
+      let altura = pokemon.height;
+      let  peso = pokemon.weight;
+      let list = '<span><b>tipo:</b>';
+      for(let t=0;t<pokemon.types.length;t++){
+        list += ' &curren;' + pokemon.types[t].type.name + '<br>';
+      }
+      list += '</span>'
+      $('#mostar-pokemon').html(
+        `<p class="mostrarNombre">${nombre}</p>
+        <img class="elejido" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeId}.png"></img>
+        ${list}
+        <span><b>Altura:</b> ${altura}</span><br>
+        <span><b>peso:</b> ${peso}</span>  
+        `)
+      console.log(nombre);
+    });  
+  }) 
+}); 
